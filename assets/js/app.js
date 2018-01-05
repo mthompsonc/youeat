163d62ef2609030500af696e5d95ad372d3e935a
Jocelyn1912 <toty_2410@hotmail.com>
2018-01-05 01:39:10 -0300

    Modificaciones mobile friendly, seguir usuarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,80 +1,70 @@
-/*
-* Función para crear registro de usuario en Firebase
-*/
+/** Función para crear registro de usuario en Firebase*/
 
 function registrar(){
   var email= document.getElementById('email').value;
   var password= document.getElementById('password').value;
 
   firebase.auth().createUserWithEmailAndPassword(email, password).then(()=>{
-     location.reload();
+    location.reload();
   }).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-  alert(errorMessage)
-});
-
+    /** Handle Errors here. */
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    alert(errorMessage)
+  });
 }
 
-/*
-* Función para identificar sign in en Firebase
-*/
+/** Función para identificar sign in en Firebase. */
 
 function ingreso(){
   var email2= document.getElementById('email2').value;
   var password2= document.getElementById('password2').value;
   firebase.auth().signInWithEmailAndPassword(email2, password2).then(()=>{
-     location.reload();
+    location.reload();
   }).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-
-  alert(errorMessage);
-  // ...
-});
+    /** Handle Errors here. */
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage);
+    // ...
+  });
 }
 
-/*
-* Función de Firebase que observa qué sucede con el usuario, si se conectó o no, si existe verificación del email, etc.
-*/
+/** Función de Firebase que observa qué sucede con el usuario, si se conectó o no, si existe verificación del email, etc. */
 
 function observador(){
   firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    aparece();
-    console.log('existe usuario activo')
-    useractive();
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    console.log('no existe usuario activo')
-    nouser();
-  }
-});
+    if (user) {
+      aparece();
+      console.log('existe usuario activo')
+      useractive();
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      console.log('no existe usuario activo')
+      nouser();
+    }
+  });
 }
 
 observador();
 
-/*
-* Esta función reemplaza botones, siempre y cuando el usuario esté logueado.
-*/
+/** Esta función reemplaza botones, siempre y cuando el usuario esté logueado. */
 
 function aparece(){
   console.log("Holi holi aparece aparece")
   var contenedor= document.getElementById('menu');
   contenedor.innerHTML= `
   <li><a href="#" id="home">Inicio</a></li>
-  <li><a href="#" id="profile">Perfil</a></li>
+  <li><a href="#" id="profile">Perfiles</a></li>
   <li><a href="#" id="close" onclick="cerrar()">Cerrar Sesión</a></li>
   `
 
@@ -93,24 +83,20 @@ function aparece(){
 
 }
 
-/*
-* Función para cerrar sesión
-*/
+/** Función para cerrar sesión. */
 
 function cerrar(){
   firebase.auth().signOut()
   .then(function(){
     console.log('Saliendo..');
-     location.reload();
+    location.reload();
   })
   .catch(function(error){
     console.log(error);
   })
 }
 
-/*
-* Se activa cuando la función observador (de Firebase) nota que no hay usuario logueado.
-*/
+/** Se activa cuando la función observador (de Firebase) nota que no hay usuario logueado. */
 
 function nouser(){
   $('#aboutus').click(function(){
@@ -120,9 +106,7 @@ function nouser(){
   })
 }
 
-/*
-* Se activa cuando la función observador (de Firebase) nota que hay un usuario logueado.
-*/
+/** Se activa cuando la función observador (de Firebase) nota que hay un usuario logueado. */
   function useractive(){
     $('#about').hide();
     $('#myHome').show();
@@ -130,9 +114,7 @@ function nouser(){
   }
 
 
-/*
-* Función que se activa al cargar el documento.
-*/
+/** Función que se activa al cargar el documento. */
 
 $(document).ready(function(){
 
@@ -141,31 +123,29 @@ $(document).ready(function(){
   $('#profile2').hide();
 })
 
-/*
-* Función para postear recetas
-*/
+/** Función para postear recetas. */
 
-  $('#post').click(function(){
-    //Guardando todos los valores en una variable.
-    var name= $('#title').val();
-    var photo= $('#urlInput').val();
-    var portion= $('#portion').val();
-    var ingredients= $('#ingredients').val();
-    var recipe= $('#recipe').val();
-    var categorias1= $('#categorias1').val();
-    //Vaciando los  input.
-    $('#title').val('');
-    $('#urlInput').val('');
-    $('#portion').val('');
-    $('#ingredients').val('');
-    $('#recipe').val('');
-    $('categorias1').val('0');
+$('#post').click(function(){
+  /** Guardando todos los valores en una variable. */
+  var name= $('#title').val();
+  var photo= $('#urlInput').val();
+  var portion= $('#portion').val();
+  var ingredients= $('#ingredients').val();
+  var recipe= $('#recipe').val();
+  var categorias1= $('#categorias1').val();
+  /** Vaciando los input. */
+  $('#title').val('');
+  $('#urlInput').val('');
+  $('#portion').val('');
+  $('#ingredients').val('');
+  $('#recipe').val('');
+  $('categorias1').val('0');
 
-    //Contenedor donde irán los nuevos comentarios.
-    var contenedor= $('.newpost');
-    //Ingresando comentario en una caja de comentario.
-    contenedor.append('<div class=box> <h3 class= text-center>'+ name + '</h3> <div> <img class="photobox center-block" src= ' + photo + '  alt=...> </div> <h6><b>Porciones:</b> ' + portion + '</h6> <p><b> Ingredientes:</b> ' + ingredients + '</p> <p><b> Instrucciones:</b> ' + recipe + '</p><p><b> Tipo de Comida:</b> ' + categorias1 + '</p></div>' );
-  })
+  /** Contenedor donde irán los nuevos comentarios. */
+  var contenedor= $('.newpost');
+  /** Ingresando comentario en una caja de comentario. */
+  contenedor.append('<div class=box> <h3 class= text-center>'+ name + '</h3> <div> <img class="photobox center-block" src= ' + photo + '  alt=...> </div> <h6><b>Porciones:</b> ' + portion + '</h6> <p><b> Ingredientes:</b> ' + ingredients + '</p> <p><b> Instrucciones:</b> ' + recipe + '</p><p><b> Tipo de Comida:</b> ' + categorias1 + '</p></div>' );
+})
 
 /** Función que muestra las recetas*/
 
@@ -178,11 +158,15 @@ var bebestible = $('.bebestible');
 var panaderiaPasteleria = $('.panaderiaPasteleria');
 var everyone = $('.everyone');
 
-//evento para que funcione el select
+/** Evento que dá funcionalidad al select. */
+
 $('#categorias').on('change', function(){
 
-  //guardo el valor del select en la variable selection
+  /** guardando el valor del select en la variable selection. */
+
   var selection = $('#categorias').val();
+
+  /** Filtro de los platos de fondo. */
 
   if (selection === 'platoFondo') {
     for (var i = 0; i < data.length; i++){
@@ -192,13 +176,11 @@ $('#categorias').on('change', function(){
         panaderiaPasteleria.children().remove();
         everyone.children().remove();
         platoFondo.append('<div class=box> <h3 class= text-center>'+ data[i].nombre + '</h3> <div> <img class="photobox center-block" src="assets/' + data[i].foto + '"> </div> <h6><b>Porciones:</b> ' + data[i].porciones + '</h6> <p><b> Ingredientes:</b> ' + data[i].ingredientes + '</p> <p><b> Instrucciones:</b> ' + data[i].receta + '</p><p><b> Tipo de Comida:</b> ' + data[i].tipo + '</p></div>')
-
-                        /*
-( );
-                        */
       }
     }
   }
+
+  /** Filtro de los postres. */
 
   if (selection === 'postre') {
     for (var i = 0; i < data.length; i++){
@@ -212,6 +194,8 @@ $('#categorias').on('change', function(){
     }
   }
 
+  /** Filtro de los platos de los bebestibles. */
+
   if (selection === 'bebestible') {
     for (var i = 0; i < data.length; i++){
       if (data[i].tipo === 'bebestible') {
@@ -224,6 +208,8 @@ $('#categorias').on('change', function(){
     }
   }
 
+  /** Filtro de la Patelería/panadería. */
+
   if (selection === 'panaderiaPasteleria') {
     for (var i = 0; i < data.length; i++){
       if (data[i].tipo === 'panaderia/pasteleria') {
@@ -235,6 +221,8 @@ $('#categorias').on('change', function(){
       }
     }
   }
+
+  /** Filtro de todas las recetas. */
 
   if (selection === 'everyone') {
     platoFondo.children().remove();
